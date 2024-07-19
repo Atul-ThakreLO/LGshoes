c3364a64a5881f2585ade245f7f60f38d55c58e0
Atul-ThakreLO <atulthakrelrat3094@gmail.com>
2024-07-19 10:19:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,566 @@
+const items = [
+  {
+    src: "./images/Nike Air Max Impact 4 png.png",
+    brand: "NIKE",
+    name: "Air Max",
+    shoeName: "Nike Air Max Impact 4",
+    shoeColor: "Black/Bright Crimson/Wolf Grey/White",
+    Shoesize: ["UK 6", "UK 7", "UK 8", "UK 10.5", "UK 11", "UK 12"],
+    type: "Impact 4",
+    detailsList: [
+      "Synthetic upper",
+      "PU midsole",
+      "Rubber outsole",
+      "Mesh Vamp",
+      "PU coated leather toe and heel overlays",
+    ],
+    materialDetails: [
+      "Midsole: 100% Synthetic",
+      "Sockliner: 100% Textile",
+      "Outsole: 100% Rubber",
+      "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+      "Lining: 100% Textile",
+    ],
+    deatils:
+      "Elevate your game and your hops. Charged with Max Air cushioning in the heel, this lightweight, secure shoe helps you get off the ground confidently and land comfortably. Plus, rubber wraps up the sides for added durability and stability.",
+    price: "10,335",
+    offer: 20,
+    fprice: "8,495",
+    total: "8,595",
+    color: "#e04102",
+  },
+  {
+    src: "./images/RS-X-Brand-Love-Unisex-Sneakers png.png",
+    brand: "PUMA",
+    name: "RS-X",
+    shoeName: "RS-X-Brand-Love-Unisex-Sneakers",
+    shoeColor: "PUMA White-Yellow Sizzle",
+    type: "Brand Love",
+    Shoesize: ["UK 6", "UK 7", "UK 8", "UK 10.5", "UK 11", "UK 12"],
+    detailsList: [
+      "Synthetic upper",
+      "PU midsole",
+      "Rubber outsole",
+      "Mesh Vamp",
+      "PU coated leather toe and heel overlays",
+    ],
+    materialDetails: [
+      "Midsole: 100% Synthetic",
+      "Sockliner: 100% Textile",
+      "Outsole: 100% Rubber",
+      "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+      "Lining: 100% Textile",
+    ],
+    deatils:
+      "The RS-X is back, with its future retro shape, fresh mix of materials and bold colour scheme.",
+    price: "12,999",
+    offer: 20,
+    fprice: "10,999",
+    total: "11,099",
+    color: "orange",
+  },
+  {
+    src: "./images/addidas_Kaptir_3.0 PNG.png",
+    brand: "Addidas",
+    name: "Kaptir",
+    shoeName: "Addidas Kaptir 3.0",
+    shoeColor: "Core Black / Screaming Orange / Shadow Olive",
+    Shoesize: ["UK 6", "UK 7", "UK 8", "UK 10.5", "UK 11", "UK 12"],
+    type: "3.0 Sneakers",
+    detailsList: [
+      "Synthetic upper",
+      "PU midsole",
+      "Rubber outsole",
+      "Mesh Vamp",
+      "PU coated leather toe and heel overlays",
+    ],
+    materialDetails: [
+      "Midsole: 100% Synthetic",
+      "Sockliner: 100% Textile",
+      "Outsole: 100% Rubber",
+      "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+      "Lining: 100% Textile",
+    ],
+    deatils:
+      "Nothing you do is regular, least of all your shoe game. Make each move a step of originality with these adidas shoes. They combine unique patterns and splashes of colour to create an energy that's entirely your own",
+    price: "5,999",
+    offer: 20,
+    fprice: "4,500",
+    total: "4,600",
+    color: "#f57e42",
+  },
+  {
+    src: "./images/Nike Air Max Excee PNG.png",
+    brand: "NIKE",
+    name: "Air Max",
+    shoeName: "Nike Air Max Excee",
+    shoeColor: "White/Dusty Cactus/Laser Orange/Thunder Blue",
+    Shoesize: ["UK 6", "UK 7", "UK 8", "UK 10.5", "UK 11", "UK 12"],
+    type: "Excee",
+    detailsList: [
+      "Synthetic upper",
+      "PU midsole",
+      "Rubber outsole",
+      "Mesh Vamp",
+      "PU coated leather toe and heel overlays",
+    ],
+    materialDetails: [
+      "Midsole: 100% Synthetic",
+      "Sockliner: 100% Textile",
+      "Outsole: 100% Rubber",
+      "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+      "Lining: 100% Textile",
+    ],
+    deatils:
+      "Get into the groove with the Nike Air Max Excee. Inspired by the Nike Air Max 90, these kicks deliver a modern twist on a legendary icon through vibrant colours, elongated design lines and distorted proportions for style that defies time.",
+    price: "10,049",
+    offer: 20,
+    fprice: "8,649",
+    total: "8,749",
+    color: "#f54251",
+  },
+  {
+    src: "./images/Clarks ClarksPro Lace Navy Leather.png",
+    brand: "CLARKS",
+    name: "ClarksPro",
+    shoeName: "Clarks ClarksPro Lace Navy Leather",
+    shoeColor: "Navy Leather",
+    Shoesize: ["UK 6", "UK 7", "UK 8", "UK 10.5", "UK 11", "UK 12"],
+    type: "Lace Navy Sneakers",
+    detailsList: [
+      "Synthetic upper",
+      "PU midsole",
+      "Rubber outsole",
+      "Mesh Vamp",
+      "PU coated leather toe and heel overlays",
+    ],
+    materialDetails: [
+      "Midsole: 100% Synthetic",
+      "Sockliner: 100% Textile",
+      "Outsole: 100% Rubber",
+      "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+      "Lining: 100% Textile",
+    ],
+    deatils:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem repellendus labore repellat ullam. Sequi maxime quis error facilis odit. Eum aliquid adipisci nesciunt obcaecati architecto fugit quiillum quaerat quis?",
+    price: "10,999",
+    offer: 20,
+    fprice: "8,300",
+    total: "8,400",
+    color: "#303a52",
+  },
+  // {
+  //   src: "./images/Nike 90 Ultra.png",
+  //   brand: "NIKE",
+  //   name: "Air Max",
+  //   shoeName: "Nike 90 Ultra",
+  //   shoeColor: "WhiteSmoke/ Gray",
+  //   Shoesize: ["UK 6", "UK 7", "UK 8", "UK 9", "UK 10", "UK 11"],
+  //   type: "90 Ultra",
+  //   detailsList: [
+  //     "Synthetic upper",
+  //     "PU midsole",
+  //     "Rubber outsole",
+  //     "Mesh Vamp",
+  //     "PU coated leather toe and heel overlays",
+  //   ],
+  //   materialDetails: [
+  //     "Midsole: 100% Synthetic",
+  //     "Sockliner: 100% Textile",
+  //     "Outsole: 100% Rubber",
+  //     "Upper: 48% Synthetic, 23% Textile, 22% Leather, 7% Rubber",
+  //     "Lining: 100% Textile",
+  //   ],
+  //   deatils:
+  //     "Nothing as fly, nothing as comfortable, nothing as proven. The Air Max 90 stays true to its running roots with the iconic Waffle sole, stitched overlays and classic details, while Max Air cushioning adds comfort to the journey.",
+  //   price: 1000,
+  //   offer: 20,
+  //   total: 850,
+  //   color: "gray",
+  // },
+];
+
+let container = document.querySelector("main .conatainer");
+const wrapper = document.querySelector(".wrapper");
+const xyz = document.querySelector(".details-list");
+items.forEach((item) => {
+  const itemDiv = document.createElement("div");
+  const slideDiv = document.createElement("div");
+  slideDiv.classList.add("image");
+  itemDiv.innerHTML = ` <div class="items">
+    <div class="info">
+        <h1 class="name"><span> ${item.brand} </span>${item.name}</h1>
+        <p class="type">${item.type}</p>
+        <div class="details">
+        ${item.deatils}
+        </div>
+        <div class="buy">
+            <button>Grab Now</button>
+        </div>
+    </div>
+    <div class="image"><img src="${item.src}" alt="shoes"></div>
+    <div class="productDetails display">
+      <div class="conatainer">
+        <h1>Product Details</h1>
+        <div class="detailsContainer">
+          <div class="name-color">
+            <h1 class="name">${item.shoeName}</h1>
+            <p class="price"><b>Rs. ${item.fprice} </b> <span>Rs. ${
+    item.price
+  } </span> <br> Price Includes GST</p>
+            <div class="color">
+              <h2>Color: </h2>
+              <p>${item.shoeColor}</p>
+            </div>
+          </div>
+          <hr>
+          <div class="size">
+            <p><b>Select Your Size</b></p>
+            <div class="size-boxes">
+            <div class="Sbox">${item.Shoesize[0]}</div>
+            <div class="Sbox">${item.Shoesize[1]}</div>
+            <div class="Sbox">${item.Shoesize[2]}</div>
+            <div class="Sbox">${item.Shoesize[3]}</div>
+            <div class="Sbox">${item.Shoesize[4]}</div>
+            <div class="Sbox">${item.Shoesize[5]}</div>
+            </div>
+          </div>
+          <hr>
+          <div class="discription">
+            <h2>Discription</h2>
+            <p>${item.deatils}</p>
+          </div>
+          <hr>
+          <div class="shipping-policy">
+            <h2>Shipping & Return</h2>
+            <p class="shipping-return">
+              Free return for all qualifying orders within 14 days of your order
+              delivery date. Visit our Return Policy for more information.
+            </p>
+          </div>
+          <hr>
+          <div class="material-details">
+            <p><b>Details</b></p>
+            <ul class="details-list">
+              <li>${item.detailsList[0]}</li>
+              <li>${item.detailsList[1]}</li>
+              <li>${item.detailsList[2]}</li>
+              <li>${item.detailsList[3]}</li>
+              <li>${item.detailsList[4]}</li>
+            </ul>
+            <p><b>Material Details</b></p>
+            <ul class="material-list">
+              <li>${item.materialDetails[0]}</li>
+              <li>${item.materialDetails[1]}</li>
+              <li>${item.materialDetails[2]}</li>
+              <li>${item.materialDetails[3]}</li>
+              <li>${item.materialDetails[4]}</li>
+            </ul>
+          </div>
+        </div>
+        <form action="" class="address">
+          <div class="field">
+            <label for="pin">PIN Code:</label>
+            <input type="number" name="pin" id="pin">
+          </div>
+          <div class="field">
+            <label for="area">Area:</label>
+            <input type="text" name="area" id="area">
+          </div>
+          <div class="field">
+            <label for="no">House No./Office/Building No. :</label>
+            <input type="text" name="no" id="no">
+          </div>
+            <label for="payment">Select Payment Method: </label>
+          <div class="payment-method">
+            <input type="radio" name="payment" id="cash">
+            <label for="cash">Cash on Delivery</label><br>
+            <input type="radio" name="payment" id="upi">
+            <label for="upi">UPI</label><br>
+            <input type="radio" name="payment" id="card">
+            <label for="card">Credit/Debit Card</label>
+          </div>
+        </form>
+        <div class="box">
+              <table>
+                  <tr>
+                      <th>Price</th>
+                      <td>${item.price}</td>
+                  </tr>
+                  <tr>
+                      <th>offer</th>
+                      <td>${item.offer}%</td>
+                  </tr>
+                  <tr>
+                      <th>Taxes</th>
+                      <td>200</td>
+                  </tr>
+                  <tr>
+                      <th>Divlivery Charges</th>
+                      <td>100</td>
+                  </tr>
+                  <tr>
+                      <th>Total</th>
+                      <td>${item.total}</td>
+                  </tr>
+              </table>
+        </div>
+        <div class="confirm">
+          <button>Confirm order</button>
+        </div>
+      </div>
+    </div>
+</div>`;
+
+  slideDiv.innerHTML = `
+  <img src=" ${item.src} " alt="">
+  `;
+  container.appendChild(itemDiv);
+  wrapper.appendChild(slideDiv);
+});
+
+const firstChild = document.createElement("div");
+const lastChild = document.createElement("div");
+firstChild.classList.add("image");
+lastChild.classList.add("image");
+firstChild.innerHTML = `<img src=" ${items[0].src} " alt="">`;
+lastChild.innerHTML = `<img src=" ${items[items.length - 1].src} " alt="">`;
+
+wrapper.prepend(lastChild);
+wrapper.appendChild(firstChild);
+
+document.documentElement.style.setProperty(`--color`, `${items[0].color}`);
+
+const itemsOfDoc = document.querySelectorAll(".items");
+const slideImages = document.querySelectorAll(".image");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const buttons = document.querySelectorAll(".buttons button");
+// const wrapper = document.querySelector(".wrapper");
+const scrollAmount =
+  document.querySelectorAll(".wrapper .image")[0].offsetWidth;
+console.log("scroll " + scrollAmount);
+
+for (let i = 1; i < itemsOfDoc.length; i++) {
+  itemsOfDoc[i].style.display = "none";
+}
+
+let pointN = 1;
+let pointP = 0;
+let colorCount = 1;
+let scrollCount;
+let inCount = 0;
+let deCount = 0;
+
+next.addEventListener("click", () => {
+  removeGrab();
+  showNext();
+  // showNextLoop();
+  increaseCount();
+});
+
+prev.addEventListener("click", () => {
+  removeGrab();
+  showPrev();
+  // showPrevLoop();
+  decreaseCount();
+});
+
+function showNext() {
+  if (pointN === itemsOfDoc.length) {
+    pointN = 0;
+  }
+  itemsOfDoc[pointN].style.display = "flex";
+  if (pointN != 0) {
+    itemsOfDoc[pointN - 1].style.display = "none";
+  }
+  if (pointN === 0) {
+    itemsOfDoc[items.length - 1].style.display = "none";
+  }
+  console.log(pointN);
+  pointP = pointN - 1;
+  colorCount = pointN - 1;
+  colorChange(pointN);
+  scrollNext(pointN);
+  if (pointN === itemsOfDoc.length - 1) {
+    pointN = 0;
+    return;
+  }
+  pointN++;
+}
+
+// function showNextLoop() {
+//   for (let i = 0; i < itemsOfDoc.length; i++) {
+//     itemsOfDoc[i].style.display = "none";
+//   }
+//   itemsOfDoc[pointN].style.display = "flex";
+
+//   // Update tracking variables and call other functions (same as before)
+//   pointP = pointN - 1;
+//   colorCount = pointN - 1;
+//   colorChange(pointN);
+//   scrollNext(pointN);
+
+//   pointN++;
+//   if (pointN === itemsOfDoc.length) {
+//     pointN = 0;
+//   }
+// }
+
+function showPrev() {
+  console.log(pointP);
+  if (pointP === -1) {
+    pointP = itemsOfDoc.length - 1;
+    itemsOfDoc[0].style.display = "none";
+  }
+  if (pointP === 0) {
+  }
+  console.log(pointP);
+  itemsOfDoc[pointP].style.display = "flex";
+  if (pointP < itemsOfDoc.length - 1) {
+    itemsOfDoc[pointP + 1].style.display = "none";
+  }
+  colorChange(pointP);
+  console.log("scroll " + pointP);
+  scrollPrev(pointP);
+  pointN = pointP + 1;
+  colorCount = pointP + 1;
+  pointP--;
+}
+
+// function showPrevLoop() {
+//   for (let i = itemsOfDoc.length - 1; i >= 0; i--) {
+//     itemsOfDoc[i].style.display = "none";
+//   }
+//   itemsOfDoc[pointP].style.display = "flex";
+
+//   // Update tracking variables and call other functions (same as before)
+//   colorChange(pointP);
+//   console.log("scroll " + pointP);
+//   scrollPrev(pointP);
+
+//   pointN = pointP + 1;
+//   colorCount = pointP + 1;
+//   pointP--;
+
+//   // Handle looping at the beginning (optional)
+//   if (pointP === -1) {
+//     pointP = itemsOfDoc.length - 1;
+//     itemsOfDoc[0].style.display = "none";
+//   }
+// }
+
+let behav = "smooth";
+function scrollNext(count) {
+  // wrapper.scrollLeft = (count * scrollAmount) / 3;
+  if (count === itemsOfDoc.length - 1) {
+    document.documentElement.style.setProperty("--leftAmount", `${-45.5}%`);
+    // behav = "instant";
+  } else {
+    document.documentElement.style.setProperty("--leftAmount", `${-50}%`);
+  }
+  console.log(count);
+  wrapper.scrollTo({
+    left: count * scrollAmount + 10,
+    behavior: behav,
+  });
+  if (count === itemsOfDoc.length - 1) {
+    // document.documentElement.style.setProperty("--leftAmount", `${-45.5}%`);
+    behav = "instant";
+  } else {
+    // document.documentElement.style.setProperty("--leftAmount", `${-50}%`);
+    behav = "smooth";
+  }
+}
+function scrollPrev(count) {
+  // wrapper.scrollLeft = (count * scrollAmount) / 3;
+  if (count === itemsOfDoc.length - 1) {
+    document.documentElement.style.setProperty("--leftAmount", `${-45.5}%`);
+    behav = "instant";
+  } else {
+    document.documentElement.style.setProperty("--leftAmount", `${-50}%`);
+    behav = "smooth";
+  }
+  wrapper.scrollTo({
+    left: count * scrollAmount + 10,
+    behavior: behav,
+  });
+}
+function colorChange(count) {
+  console.log("calling");
+  document.documentElement.style.setProperty(
+    `--color`,
+    `${items[count].color}`
+  );
+}
+
+const grabButton = document.querySelectorAll(".buy button");
+const orderDetails = document.querySelectorAll(".productDetails");
+const info = document.querySelectorAll(".info");
+const itemsImage = document.querySelectorAll(".items .image");
+const smallSlider = document.querySelector(".small-slider");
+const buttonsALL = document.querySelector(".buttons");
+const backButton = document.querySelector(".backButton");
+
+grabButton.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    itemsImage[index].classList.add("grab");
+    itemsImage[index].classList.remove("removeGrab");
+    info[index].classList.add("display");
+    orderDetails[index].classList.remove("display");
+    smallSlider.classList.add("display");
+    buttonsALL.classList.add("display");
+    backButton.classList.remove("display");
+  });
+  backButton.addEventListener("click", () => {
+    itemsImage[index].classList.remove("grab");
+    itemsImage[index].classList.add("removeGrab");
+    info[index].classList.remove("display");
+    orderDetails[index].classList.add("display");
+    smallSlider.classList.remove("display");
+    buttonsALL.classList.remove("display");
+    backButton.classList.add("display");
+  });
+});
+
+function removeGrab() {
+  itemsImage.forEach((image) => {
+    if (image.classList.contains("removeGrab")) {
+      image.classList.remove("removeGrab");
+    }
+  });
+}
+
+// grabButton.forEach((button, index) => {
+//   const addDisplayItems = [
+//     orderDetails[index],
+//     info[index],
+//     smallSlider,
+//     buttonsALL,
+//     backButton,
+//   ];
+//   button.addEventListener("click", () => {
+//     console.log("clicked " + index);
+//     console.log(orderDetails[index].classList);
+//     i = index;
+//     itemsImage[index].classList.add("grab");
+//     itemsImage[index].classList.remove("removeGrab");
+//     addDisplayItems.forEach((item) => {
+//       item.classList.toggle("display");
+//     });
+//     // orderDetails[index].classList.toggle("display");
+//     // info[index].classList.toggle("display");
+//   });
+//   backButton.addEventListener("click", () => {
+//     // console.log("clicked " + index);
+//     addDisplayItems.forEach((item) => {
+//       item.classList.toggle("display");
+//       console.log(item);
+//     });
+//     itemsImage[index].classList.remove("grab");
+//     itemsImage[index].classList.add("removeGrab");
+//     // setTimeout(() => {
+//     //   itemsImage[index].classList.remove("removeGrab");
+//     // },600)
+//   });
+// });
